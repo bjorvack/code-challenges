4bf53b8b4ad9346dce83c319cc4ddec64b2d3612
Bjorn <bjorn@sumocoders.be>
2017-11-23 10:51:14 +0100

    Add comment to the fragment shader

--- web-gl/engine/shaders/fragment-shader.glsl
+++ web-gl/engine/shaders/fragment-shader.glsl
@@ -4,6 +4,7 @@
 // to pick one. mediump is a good default. It means "medium precision"
 precision highp float;
 
+// input color
 uniform vec4 u_color;
 
 // we need to declare an output for the fragment shader
